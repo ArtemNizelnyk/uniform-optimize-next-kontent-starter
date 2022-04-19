4fc91559fb9744b494601378218f5212afa52812
ArtemNizelnyk <artem.nizelnik@gmail.com>
2022-04-19 14:38:23 +0300

    1

--- components/PersonalizedHero.tsx
+++ components/PersonalizedHero.tsx
@@ -1,7 +1,6 @@
-import { Personalize } from "@uniformdev/optimize-tracker-react";
 import { PersonalizedHeroData } from "../lib/types";
 import { Hero } from "./Hero";
 
 export const PersonalizedHero = ({ item }: { item: PersonalizedHeroData }) => {
-  return <Personalize variations={item.heros} component={Hero} />;
+  return <div> personalized </div>;
 };
